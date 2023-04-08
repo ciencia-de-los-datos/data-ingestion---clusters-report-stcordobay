746b2f7c7129da17e6cd6e6813eb0321a471f5b2
stcordobay <scordobay@unal.edu.co>
2023-04-08 14:56:27 -0500

    EjercicioIngestion

--- pregunta.py
+++ pregunta.py
@@ -13,9 +13,20 @@ import pandas as pd
 
 
 def ingest_data():
+    dat=pd.read_fwf('./clusters_report.txt', skiprows=4, 
+                names=['cluster', 'cantidad_de_palabras_clave', 
+                       'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
 
+    dat.loc[:,'principales_palabras_clave']=dat.fillna(method='ffill').groupby('cluster')['principales_palabras_clave'].transform(lambda x: ' '.join(x))
+    
+    dat=dat.dropna().reset_index()
+    dat.loc[:,'principales_palabras_clave']=dat.principales_palabras_clave.str.replace(r'\s+', ' ', 
+                                                                                   regex=True).str.replace('.','')
+    dat=dat.drop(columns='index')
+    
+    dat['porcentaje_de_palabras_clave']=dat['porcentaje_de_palabras_clave'].str[:-2].str.replace(',','.').astype(float)
     #
     # Inserte su código aquí
     #
 
-    return df
+    return dat
